4b51fce56c55d6c31f568a0e40beb8c8625355bd
Kundumemirkr <76641628+Kundumemirkr@users.noreply.github.com>
2022-03-06 18:21:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+const ayarlar = require("./ayarlar.json");
+
+fetch(`https://id.twitch.tv/oauth2/token?client_id=${ayarlar.clientid}&client_secret=${ayarlar.secret}&grant_type=client_credentials`, {
+ method: "POST"
+}).then(res => {
+    console.log(res);
+})
+
+
+//res.json().then(response => { console.log(response); })
